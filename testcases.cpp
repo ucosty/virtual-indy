b197fa728351fbadb0fa5412a083ff5526b44710
folkert <folkert@4685b172-2c84-4122-9318-a9c62b6e2a62>
2013-02-05 15:08:42 +0000

    enhanced testcases / fixed bugs in testcases

--- testcases.cpp
+++ testcases.cpp
@@ -15,9 +15,10 @@ int main(int argc, char *argv[])
 
 	/* SRL */
 	p -> set_register(1, 13);
-	p -> set_register(2, 9);
+	int input_val = 191;
+	p -> set_register(2, input_val);
 	int instr = make_cmd_SPECIAL(2, 1, 3, 2, 0);
-	printf("instruction: %08x\n", instr);
+	// printf("instruction: %08x\n", instr);
 	if (!m -> write_32b(0, instr))
 		error_exit("failed to write to memory @ 0");
 	int temp_32b;
@@ -26,16 +27,20 @@ int main(int argc, char *argv[])
 	if (temp_32b != instr)
 		error_exit("read from/write to memory differ");
 	p -> tick();
-	if (p -> get_register(2) != 9)
-		error_exit("SRL: rt changed");
+	int input_val_check = p -> get_register(2);
+	if (input_val_check != input_val)
+		error_exit("SRL: rt changed from %d to %d", input_val, input_val_check);
 	temp_32b = p -> get_register(1);
-	if (temp_32b != 72)
-		error_exit("SRL: rd (%d) != 72", temp_32b);
+	int expected = 23;
+	if (temp_32b != expected)
+		error_exit("SRL: rd (%d) != %d", temp_32b, expected);
 
 
 	delete p;
 	delete mb;
 	delete m;
+
+	printf("all fine\n");
 
 	return 0;
 }
