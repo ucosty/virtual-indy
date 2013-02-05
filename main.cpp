bb2e61faf1e20439051ae0efc0b9a9d32e7d325c
folkert <folkert@4685b172-2c84-4122-9318-a9c62b6e2a62>
2013-02-05 15:08:18 +0000

    basic test setup / debug console / load prom into emulated ram

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <signal.h>
+#include <stdio.h>
+
+#include "error.h"
+#include "processor.h"
+#include "processor_utils.h"
+#include "utils.h"
+#include "debug_console.h"
+
+volatile bool terminate = false;
+
+void sig_handler(int sig)
+{
+        terminate = true;
+}
+
+int main(int argc, char *argv[])
+{
+	signal(SIGHUP , SIG_IGN);
+	signal(SIGTERM, sig_handler);
+	signal(SIGINT , sig_handler);
+	signal(SIGQUIT, sig_handler);
+	signal(SIGPIPE, SIG_IGN);
+
+	memory_bus *mb = new memory_bus();
+
+	memory *m = new memory(4 * 1024);
+	mb -> register_memory(0, 0xfff, m);
+
+	unsigned char *prom = NULL;
+	int prom_len = -1;
+	load_file("ip20prom.070-8116-004.BE.bin", &prom, &prom_len);
+	memory *m_prom = new memory(prom, prom_len);
+	mb -> register_memory(0xbfc00000, 0x7ffff, m_prom); // IP20, 32bit
+
+	processor *p = new processor(mb);
+	p -> set_PC(0xbfc00000 + 0x0b80); // offset 0xb80 entry point is a guess
+
+	debug_console *dc = new debug_console(p);
+
+	for(;!terminate;)
+	{
+		dc -> tick();
+		p -> tick();
+	}
+
+	delete dc;
+
+	delete p;
+	delete mb;
+	delete m;
+
+	return 0;
+}
