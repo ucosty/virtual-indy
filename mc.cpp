69459eea362f31751faf934914dcd55ff935b15e
folkert <folkert@4685b172-2c84-4122-9318-a9c62b6e2a62>
2013-02-13 19:22:57 +0000

    mc: 512MB ram

--- mc.cpp
+++ mc.cpp
@@ -22,9 +22,47 @@ void mc::read_32b(uint64_t offset, uint32_t *data)
 		// bit 2: register size (32/64)
 		// *data = 0x0f;
 	}
-	if (offset == 0x48 || offset == 0x4c)	// REF_CTR - refresh counter
+	else if (offset == 0x48 || offset == 0x4c)	// REF_CTR - refresh counter
 	{
-		*data = refresh_counter++;
+		*data = refresh_counter--;
+	}
+	else if (offset == 0xc0 || offset == 0xc4)	// MEMCFG0
+	{
+		// BASE0
+		// 31 1 served
+		// 30 1 number of subbanks
+		// 29 1 bank 0/2 valid
+		// 28/27/26/25/24 11111 sim size 8M x 36 bits, 2 subbanks
+		// 23-16 00100000 (0x8000000 >> 22), 
+		// 1111111100100000
+		// BASE1
+		// 15 1 served
+		// 14 1 number of subbanks
+		// 13 1 bank 0/2 valid
+		// 12/11/10/09/08 11111 sim size 8M x 36 bits, 2 subbanks
+		// 07-00 01000000 (0x10000000 >> 22), 
+		// 1111111101000000
+		// 11111111001000001111111101000000
+		*data = 0xFF20FF40;
+	}
+	else if (offset == 0xc8 || offset == 0xcc)	// MEMCFG1
+	{
+		// BASE0
+		// 31 1 served
+		// 30 1 number of subbanks
+		// 29 1 bank 0/2 valid
+		// 28/27/26/25/24 11111 sim size 8M x 36 bits, 2 subbanks
+		// 23-16 01000000 (0x20000000 >> 22), 
+		// 1111111101000000
+		// BASE1
+		// 15 1 served
+		// 14 1 number of subbanks
+		// 13 1 bank 0/2 valid
+		// 12/11/10/09/08 11111 sim size 8M x 36 bits, 2 subbanks
+		// 07-00 10000000 (0x30000000 >> 22), 
+		// 1111111110000000
+		// 11111111010000001111111110000000
+		*data = 0xFF40FF80;
 	}
 	else
 	{
