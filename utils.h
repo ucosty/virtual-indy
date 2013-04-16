4fcd984f4f6c009408932eeef9d840e2708989c0
folkert <folkert@4685b172-2c84-4122-9318-a9c62b6e2a62>
2013-04-16 18:47:58 +0000

    added to_bin_str() which converts a value to a binary string

--- utils.h
+++ utils.h
@@ -3,3 +3,4 @@
 std::string format(const char *fmt, ...);
 void load_file(std::string filename, unsigned char **data, uint64_t *len);
 double get_ts();
+std::string to_bin_str(uint64_t value, uint8_t bits);
